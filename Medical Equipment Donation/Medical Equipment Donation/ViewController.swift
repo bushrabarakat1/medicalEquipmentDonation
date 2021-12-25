e8678136153003bedabc1e88df05546033b82386
bushrabarakat1 <Bushra.Barakat.jiz.s@tuwaiq.edu.sa>
2021-12-25 20:56:27 +0300

    init x code project structure

---
+++ Medical Equipment Donation/Medical Equipment Donation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Medical Equipment Donation
+//
+//  Created by Bushra Barakat on 21/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
